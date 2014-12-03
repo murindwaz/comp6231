9dbfe493f7a97ddb1e3010e79a1fdab2868e66c0
Pascal Maniraho <murindwaz@yahoo.fr>
2014-12-02 21:50:03 -0500

    Updated the StringTransformer.java

---
+++ src/main/java/ca/concordia/drms/util/StringTransformer.java
@@ -0,0 +1,53 @@
+package ca.concordia.drms.util;
+
+import com.google.gson.Gson;
+
+import ca.concordia.drms.model.*;
+
+
+
+
+/**
+ * This utility will take an object and transform it into a valid JSON String 
+ * @author murindwaz
+ */
+public class StringTransformer {
+	
+	
+	/**
+	 * Helper to transform an object into a JSON string
+	 * @param Book book
+	 * @return String 
+	 */
+	public static String getString(Book book){
+		return (new Gson()).toJson(book);
+	}
+	
+	/**
+	 * Helper that transforms an Account into a JSON string
+	 * @param Account account
+	 * @return String 
+	 */
+	public static String getString(Account account ){
+		return (new Gson()).toJson(account);
+	}
+	
+	/**
+	 * Helper that transforms a Reservation Object into a JSON String
+	 * @param reservation
+	 * @return
+	 */
+	public static String getString(Reservation reservation){
+		return (new Gson()).toJson(reservation);
+	}
+	
+	/**
+	 * Helper that transforms a NetworkMessage into a JSON String
+	 * @param networkMessage
+	 * @return
+	 */
+	public static String getString(NetworkMessage networkMessage){
+		return (new Gson()).toJson(networkMessage);
+	}
+	
+}
